74c0980416d894bdabd77e1f0e1af2582a3e855c
Bryt Li <bryt@oriente.com>
2018-02-27 15:05:02 +0800

    update

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ var params = {
     wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec. 
     mount: [['/components', './node_modules']], // Mount a directory to a route. 
     logLevel: 0, // 0 = errors only, 1 = some, 2 = lots 
+    htpasswd: 'htpasswd',
     middleware: [outputPlantumlImage] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack 
 };
 
